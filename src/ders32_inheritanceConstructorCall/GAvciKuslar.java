292cac42754ad1e7c6238ef1a3f42e7e24a39e39
{ismailsari} <{ismailsari344@gmail.com}>
2022-11-10 16:51:06 +0300

    inheritanceConstructorCall

--- src/ders32_inheritanceConstructorCall/GAvciKuslar.java
+++ src/ders32_inheritanceConstructorCall/GAvciKuslar.java
@@ -16,6 +16,10 @@ public class GAvciKuslar extends FKuslar {
 
         Ama data turunu
         o objenin bagli oldugu class'lardan secebiliriz
+
+        Eger constructor ve data turu farkli secilirse
+        bu durumda class uyesi olan
+        variable ve method'lar farkli davranislar gosterirler
          */
 
     }
